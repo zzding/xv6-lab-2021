e3b9e64242dd1171f671f8165862cc1ef8ee8f3a
zzder <zzder@mail.nwpu.edu.cn>
2023-05-02 16:34:11 +0800

    [特性新增] lab1中的find模块

--- user/find.c
+++ user/find.c
@@ -3,66 +3,73 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-void find_helper(char const *path, char const *target)
-{
-	char buf[512], *p;
-	int fd;
-	struct dirent de;
-	struct stat st;
-	if((fd = open(path, 0)) < 0){
-		fprintf(2, "find: cannot open %s\n", path);
-		exit(1);
-	}
-
-	if(fstat(fd, &st) < 0){
-		fprintf(2, "find: cannot stat %s\n", path);
-		exit(1);
-	}
-
-	switch(st.type){
-		case T_FILE:
-			fprintf(2, "Usage: find dir file\n");
-			exit(1);
-		case T_DIR:
-			if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-				printf("find: path too long\n");
-				break;
-			}
-			strcpy(buf, path);
-			p = buf + strlen(buf);
-			*p++ = '/';
-			while(read(fd, &de, sizeof(de)) == sizeof(de)){
-				if (de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
-					continue;
-				memmove(p, de.name, DIRSIZ);
-				p[DIRSIZ] = 0;
-				if(stat(buf, &st) < 0){
-        			printf("find: cannot stat %s\n", buf);
-        			continue;
-      			}
-      			if(st.type == T_DIR){
-      				find_helper(buf, target);
-      			}else if (st.type == T_FILE){
-      				if (strcmp(de.name, target) == 0)
-      				{
-      					printf("%s\n", buf);
-      				}
-      			}
-			}
-			break;
-	}
-	close(fd);
+char* fmtname(char *path){
+    static char buf[DIRSIZ+1];
+    char *p;
+    for(p = path + strlen(path); p >= path && *p != '/'; --p);
+    p++;
+    if(strlen(p) >= DIRSIZ) 
+    return p;
+    memmove(buf, p, strlen(p));
+    memset(buf + strlen(p), ' ', DIRSIZ - strlen(p));
+    return buf;
 }
-int main(int argc, char const *argv[])
-{
-	if (argc != 3)
-	{
-		fprintf(2, "Usage: find dir file\n");
-		exit(1);
-	}
 
-	char const *path = argv[1];
-	char const *target = argv[2];
-	find_helper(path, target);
-	exit(0);
+void find(char *path, char* filename){
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
+
+    if((fd = open(path, 0)) < 0){
+        fprintf(2, "find: cannot open %s\n", path);
+        return ;
+    }
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "find: cannot stat %s\n",path);
+        close(fd);
+        return;
+    }
+    switch(st.type){
+        case T_FILE:
+            if(strcmp(fmtname(path), filename) == 0){
+                printf("%s/%s\n", path, filename);
+            }
+            break;
+        case T_DIR:
+            if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+                printf("find: path too long\n");
+                break;
+            }
+            strcpy(buf, path);
+            p = buf + strlen(buf);
+            *p++ = '/';
+            while(read(fd, &de, sizeof(de)) == sizeof(de)){
+                if (de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+					continue;
+                memmove(p, de.name, DIRSIZ);
+                p[DIRSIZ] = 0;
+                if(stat(buf, &st) < 0){
+                    printf("find: cannot stat %s\n", buf);
+                    continue;
+                }
+                if(strcmp(de.name, filename) == 0){
+                   printf("%s/%s\n", path, filename); 
+                }
+                if(st.type == T_DIR){
+                    find(buf, filename);
+                }
+            }
+            break;
+    }
+    close(fd);
+}
+
+int main(int argc, char *argv[]){
+    if(argc < 3){
+        fprintf(2, "usage: find path filename...\n");
+        exit(1);
+    }
+    find(argv[1], argv[2]);
+    exit(0);
 }
